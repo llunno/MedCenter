1f353b2ed7317b56f3982ec0907dc94f2030885a
Lucas Nogueira <lucasjesusnogueira@gmail.com>
2022-10-01 16:30:32 -0300

    update main page & router

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,16 @@
 <template>
-  <header>
-    <nav class="navbar navbar-expand-md navbar-fixed-top navbar-nav">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src="@/assets/logo.png" alt="logo-sistema" width="24" height="24" class="d-inline-block align-text-top"/>
-          MedCenter
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a href="#produtos-component" class="nav-link">Produtos</a>
-            </li>
-            <li class="nav-item">
-              <a href="#pacientes-component" class="nav-link">Pacientes</a>
-            </li>
-            <li class="nav-item">
-              <a href="#medicos-component" class="nav-link">Médicos</a>
-            </li>
-            <li class="nav-item">
-              <a href="#sobre-component" class="nav-link">Sobre</a>
-            </li>
-            <li class="nav-item">
-              <a href="#contato-component" class="nav-link">Contato</a>
-            </li>
-          </ul>
-          <div class="div-buttons">
-              <router-link class="btn btn-success text-light" to="/login">Cadastre-se</router-link>
-              <router-link type="button" class="btn btn-success text-light" to="/signup">Log-in</router-link>
-          </div>
-        </div>
-      </div>
-    </nav>
-    <router-view/>
-    <div class="home">
-      <h1>Uma solução ideal para conectar necessidade e oportunidade</h1>
-      <h2>
-        <span class="span-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</span>
-      </h2>
-    </div>
-  </header>
-  <main>
-    <produtos id="produtos-component"/>
-    <pacientes/>
-    <medicos/>
-    <sobre/>
-    <contato/>
-  </main>
+  <router-link to="/" aria-current="page"/>
+  <router-view/>
 </template>
 
 <script>
 import { onMounted, ref } from "vue";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { useRouter } from "vue-router"
-import Produtos from "@/components/Produtos";
-import Pacientes from "@/components/Pacientes";
-import Medicos from "@/components/Medicos";
-import Sobre from "@/components/Sobre";
-import Contato from "@/components/Contato";
 
 export default {
   name: 'app',
-  components: {
-    produtos: Produtos,
-    pacientes: Pacientes,
-    medicos: Medicos,
-    sobre: Sobre,
-    contato: Contato
-  }
+  
 }
 
 const router = useRouter();
@@ -97,62 +37,5 @@ const handleSignOut = () => {
 </script>
 
 <style scoped lang="scss">
-  
-  $success: #008894;
 
-  @import "../node_modules/bootstrap/scss/bootstrap";
-
-  #logo {
-    width: 40px;
-    height: 40px;
-  }
-
-  a { 
-    color: black;
-  }
-
-  .div-buttons {
-    flex-wrap: wrap;
-    display: flex;
-    gap: 1rem;
-  }
-
-  .nav-item {
-    font-weight: bold;
-    text-color: black;
-  }
-
-  header {
-    background-image: url(@/assets/doctor.jpg);
-    height: 62rem;
-    background-attachment: scroll;
-    background-size: cover;
-    width: 100%;
-    background-repeat: no-repeat;
-  }
-
-  .home h1 {
-    text-align: left;
-    align-items: center;
-    justify-content: center;
-    margin: 150px 50px 10px;
-
-  }
-
-  .home h2 {
-    text-align: left;
-    align-items: center;
-    justify-content: center;
-    margin: 0px 50px;
-  }
-
-  h1,h2 {
-    font-weight: bold;
-    margin-top: 100px;
-    margin-left: 60px;
-    max-width: 50%;
-    color: #FFF;
-    text-shadow: black 0.1em 0.1em 0.2em;
-    line-height: 1.5;
-  }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,391 +1,133 @@
 <template>
-  <div class="home">
-    <h1><p>Uma solução ideal para conectar <br>necessidade e oportunidade</p></h1>
-    <h2><span class="span-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br/>
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco <br/>laboris nisi ut aliquip ex ea commodo consequat</span></h2>
-  </div>
-  <div class="produtos">
-    <h1>Conheça nossos produtos</h1>
-    <div class="card-deck mt-5">
-      <div class="card">
-        <img src="../assets/doctor.jpg" class="card-img-top" alt="...">
-        <div class="card-body ">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  <header>
+    <nav class="navbar navbar-expand-md navbar-fixed-top navbar-nav">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="@/assets/logo.png" alt="logo-sistema" width="24" height="24" class="d-inline-block align-text-top"/>
+          MedCenter
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a href="#produtos-component" class="nav-link">Produtos</a>
+            </li>
+            <li class="nav-item">
+              <a href="#pacientes-component" class="nav-link">Pacientes</a>
+            </li>
+            <li class="nav-item">
+              <a href="#medicos-component" class="nav-link">Médicos</a>
+            </li>
+            <li class="nav-item">
+              <a href="#sobre-component" class="nav-link">Sobre</a>
+            </li>
+            <li class="nav-item">
+              <a href="#contato-component" class="nav-link">Contato</a>
+            </li>
+          </ul>
+          <div class="div-buttons">
+              <router-link class="btn btn-success text-light" to="/signup">Cadastre-se</router-link>
+              <router-link type="button" class="btn btn-success text-light" to="/login">Log-in</router-link>
+          </div>
         </div>
       </div>
-      <div class="card">
-        <img src="../assets/doctor.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-      <div class="card">
-        <img src="../assets/doctor.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
+    </nav>
+    <router-view/>
+    <div class="home">
+      <h1>Uma solução ideal para conectar necessidade e oportunidade</h1>
+      <h2>
+        <span class="span-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</span>
+      </h2>
     </div>
-    <div class="card-deck mt-5">
-      <div class="card">
-        <img src="../assets/doctor.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-      <div class="card">
-        <img src="../assets/doctor.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-      <div class="card">
-        <img src="../assets/doctor.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="pacientes pt-4">
-    <h1>Pacientes</h1>
-    <div>
-      <div class="row">
-        <div class="col-sm ml-5 pt-5">
-          <p class="text-left h1 4 pt-5 pb-3"><strong>Nosso foco é saude</strong></p>
-          <p class="text-justify pt-5 h4">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat”</p>
-        </div>
-        <div class="col-sm mr-5 ml-5 pt-5 pb-5">
-          <img class="rounded" src="../assets/pacientes.jpg" alt="">
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="medicos pt-4">
-    <h1>Médicos</h1>
-    <div>
-      <div class="row">        
-        <div class="col-sm mr-5 ml-5 pt-5 pb-5">
-          <img class="rounded" src="../assets/medico.jpg" alt="">
-        </div>
-        <div class="col-sm ml-5 pt-5">
-          <p class="text-left h1 pt-5 pb-3"><strong>Te ajudamos a mostrar a qualidade do seu trabalho</strong></p>
-          <p class="text-left text-justify pt-5 h4">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat”</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="sobre pt-4">
-    <h1>Sobre</h1>
-    <div>
-      <p class="text-justify pb-5 pt-5 pl-5 pr-5 h3">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat”</p>
-    </div>
-    <div class="row">
-      <div class="col-sm h3">
-        <h3>
-          <b>
-             Missão
-          </b>
-        </h3>
-      </div>
-      <div class="col-sm h3">
-        <h3>
-          <b>
-             Visão
-          </b>
-        </h3>
-      </div>
-      <div class="col-sm h3">
-        <h3>
-          <b>
-             Valores
-          </b>
-        </h3>
-      </div>
-    </div>
-    <div class="card-deck mt-5 pb-5">      
-      <div class="card">        
-        <div class="card-body ">
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>          
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-body">
-          <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>          
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-body">
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>          
-        </div>
-      </div>
-    </div>
-    <div>
-
-    </div>
-  </div>
-  <div class="contato">
-    <h1>Contato</h1>
-    <div>
-      <div>
-        <p class="text-justify pb-5 pt-5 pl-5 pr-5 h3">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat”</p>
-      </div>      
-    </div>    
-    <div class="">
-      <div class="row justify-content-center">
-        <div class="col-12 col-md-8 col-lg-6 pb-5">
-          <form>
-              <div class="formulario">
-                  <div class="card-header p-0">
-                    <div class="bg text-white text-center py-2">
-                      <h3><i class="fa fa-envelope"></i></h3>
-                    </div>
-                  </div>
-                  <div class="card-body p-3">
-                    <div class="form-group">
-                      <div class="input-group mb-2">
-                        <div class="input-group-prepend">
-                          <div class="input-group-text"><i class="fa fa-user"></i></div>
-                        </div>
-                        <input type="text" class="form-control" id="nombre" name="nombre" placeholder=" Nome e Sobrenome" required>
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <div class="input-group mb-2">
-                        <div class="input-group-prepend">
-                          <div class="input-group-text"><i class="fa fa-envelope"></i></div>
-                        </div>
-                        <input type="email" class="form-control" id="nombre" name="email" placeholder=" Email" required>
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <div class="input-group mb-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><i class="fa fa-comment"></i></div>
-                        </div>
-                        <textarea class="form-control" placeholder=" Descrição" required></textarea>
-                      </div>
-                    </div>
-                    <div class="text-center">
-                      <input type="submit" value="Enviar" class="btn btn-info btn-block rounded-0 py-2">
-                    </div>
-                  </div>
-              </div>
-          </form>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="footer">
-    <p>Sistema para Médicos &copy; 2022</p>
-  </div>
+  </header>
+  <main>
+    <produtos id="produtos-component"/>
+    <pacientes/>
+    <medicos/>
+    <sobre/>
+    <contato/>
+  </main>
 </template>
 
 <script>
+  import Produtos from "@/components/Produtos";
+  import Pacientes from "@/components/Pacientes";
+  import Medicos from "@/components/Medicos";
+  import Sobre from "@/components/Sobre";
+  import Contato from "@/components/Contato";
 
-export default {
-  name: 'HomeView',
+  export default {
+  name: 'app',
   components: {
-    
+    produtos: Produtos,
+    pacientes: Pacientes,
+    medicos: Medicos,
+    sobre: Sobre,
+    contato: Contato
   }
 }
 </script>
 
-<style scoped>
-  * {
-    margin:0;
-    padding:0;
+<style scoped lang="scss">
+
+  $success: #008894;
+
+  @import "../../node_modules/bootstrap/scss/bootstrap";
+
+  #logo {
+    width: 40px;
+    height: 40px;
   }
 
-  .home {    
-    background-image: url(../assets/doctor.jpg);   
-    width: 100%;    
-    height: 100vh;    
-    margin-top: -80px;
-    background-size: 100%;
+  a { 
+    color: black;
+  }
+
+  .div-buttons {
+    flex-wrap: wrap;
+    display: flex;
+    gap: 1rem;
+  }
+
+  .nav-item {
+    font-weight: bold;
+    text-color: black;
+  }
+
+  header {
+    background-image: url(@/assets/doctor.jpg);
+    height: 62rem;
+    background-attachment: scroll;
+    background-size: cover;
+    width: 100%;
     background-repeat: no-repeat;
   }
 
-  .home h1 {    
-    display: flex;
-    float: left;
+  .home h1 {
     text-align: left;
     align-items: center;
     justify-content: center;
-    margin: 40px 50px 10px;
+    margin: 150px 50px 10px;
+
   }
 
-  .home h2 {    
-    display: flex;
-    float: left;
+  .home h2 {
     text-align: left;
     align-items: center;
     justify-content: center;
     margin: 0px 50px;
   }
 
-  .home p {
+  h1,h2 {
     font-weight: bold;
     margin-top: 100px;
     margin-left: 60px;
+    max-width: 50%;
     color: #FFF;
     text-shadow: black 0.1em 0.1em 0.2em;
+    line-height: 1.5;
   }
-
-  .home span {    
-    margin-top: 100px;
-    margin-left: 60px;
-    color: #FFF;
-    text-shadow: black 0.1em 0.1em 0.2em;
-  }
-
-  .produtos {
-    background-color: #EEEEEE;
-  }
-
-  .card {
-    background-color: #008894;
-  }
-
-  .card h5 {
-    color: #EEEEEE;
-  }
-
-  .produtos h1 {
-    padding-top: 30px;
-    text-decoration: underline;
-    color: #395B59;
-  }
-
-  .pacientes {
-    background-color: #EEEEEE;
-  }
-
-  .pacientes h1 {
-    text-decoration: underline;
-    color: #395B59;
-  }
-
-  .pacientes p {
-    color: #395B59;
-  }
-
-  .pacientes img {
-    border: 3px solid #395B59;
-    width: 100%;
-    height: 100%;
-  }
-
-  .medicos {
-    background-color: #EEEEEE;
-  }
-
-  .medicos h1 {
-    text-decoration: underline;
-    color: #395B59;
-  }
-
-  .medicos p {
-    color: #395B59;
-  }
-
-  .medicos img {
-    border: 3px solid #395B59;
-    width: 100%;
-    height: 100%;
-  }
-
-  .sobre {
-    background-color: #EEEEEE;
-  }
-
-  .sobre h1 {
-    text-decoration: underline;
-    color: #395B59;
-  }
-
-  .sobre p {
-    color: #395B59;
-    line-height: 60px;
-  }
-
-  .card p {
-    color: #EEEEEE;
-    font-size: 12px;
-  }
-
-  .sobre h3 {
-    color: #395B59;
-  }
-
-  .contato {
-    background-color: #EEEEEE;
-  }
-
-  .contato h1 {
-    text-decoration: underline;
-    color: #395B59;
-  }
-
-  .contato p {
-    line-height: 60px;
-    color: #395B59;
-  }
-
-  .formulario {
-    background-color: #EEEEEE;
-    border: 1px solid #395B59;
-  }
-
-  .bg {
-    background-color: #EEEEEE;
-    color: #EEEEEE;
-    border: 1px solid #395B59;
-  }
-
-  .form-control {
-    border: 1px solid #395B59;
-  }
-
-  .btn {
-    background-color: #395B59;
-    border: 1px solid #395B59;
-  }
-
-  .fa {
-    color: #395B59;
-  }
-
-  .input-group-text {
-    color: #395B59;
-    border: 1px solid #395B59;
-  }
-
-  .footer {
-    height: 50px;
-    background-color: #008894;
-    color: #FFF;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    bottom: 0; 
-  }
-
-  .footer p {
-    color: #EEEEEE;
-  }
-
 </style>
