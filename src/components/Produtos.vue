4ca1da029b99aa97e17fcb4a2477857c10a23883
Lucas Nogueira <lucasjesusnogueira@gmail.com>
2022-10-01 16:10:46 -0300

    Adicionado Produtos e alterado front-end

---
+++ src/components/Produtos.vue
@@ -0,0 +1,71 @@
+<template>
+    <section class="container-fluid text-center">
+        <h2>Conheça nossos produtos</h2>
+        <div class="d-flex justify-content-xxl-center flex-wrap">
+            <div class="card m-2">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">“Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat”</p>
+                </div>
+            </div>
+            <div class="card m-2">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">“Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat”</p>
+                </div>
+            </div>
+            <div class="card m-2">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">“Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat”</p>
+                </div>
+            </div>
+            <div class="card m-2">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">“Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat”</p>
+                </div>
+            </div>
+            <div class="card m-2">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">“Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat”</p>
+                </div>
+            </div>
+            <div class="card m-2">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">“Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exerci ex ea commodo consequat”.</p>
+                </div>
+        </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    h2 {
+        margin-top: 40px;
+        margin-bottom: 35px;
+        text-align: center;
+        color: #395B59;
+        text-decoration: underline;
+    }
+
+    .card {
+        width: 37rem;
+        background-color: #008894;
+        color: white;
+        border: 0px;
+        border-radius: 0;
+        padding-bottom: 2.4rem;
+        padding-top: 1rem;
+    }
+
+    h5 {
+        margin-bottom: 2.2rem;
+    }
+
+    p {
+        font-size: 1.12rem;
+    }
+</style>
