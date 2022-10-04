6faac7494de52c26f3841db33e12ba780cc46f2c
Lucas Nogueira <lucasjesusnogueira@gmail.com>
2022-10-04 01:14:34 -0300

    finished dashboard

--- src/views/paciente/Dashboard.vue
+++ src/views/paciente/Dashboard.vue
@@ -1,237 +1,151 @@
 <template>
-  <div id="containerAll">
-    <header>
-      <nav class="navbar navbar-expand-lg navbar-fixed-top navbar-nav" style="background-color: #008894" id="navElement">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <router-link to="/">
-              <img src="@/assets/logo.png" alt="logo-sistema" width="24" height="24" class="d-inline-block align-text-top"/>              
-            </router-link>    
-            MedCenter
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse">
-            <div class="container-fluid" id="formContainer">
-              <form class="d-flex" role="search" @submit.prevent>
-                <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
-                <button class="btn btn-light btn-sm">Buscar</button>
-              </form>
-            </div>
-            <div class="div-User d-flex align-items-center gap-2">
-                <p class="m-0">{{user.user_metadata.nome ? user.user_metadata.nome : user.email}}</p>
-                <font-awesome-icon id="userIcon" icon="fa-solid fa-circle-user"/>
-                  <!-- Provisório, favor fazer como preferirem -->
-
-                <button class="btn btn-light btn-sm" @click="signOut">Sign-Out</button>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </header>
-    <div class="container mb-auto mt-auto" id="containerMainContent">
-      <router-link type="button" class="button medium-btn cel cel-1" to="/consultaspaciente">Minhas Consultas</router-link>
-      <router-link type="button" class="button medium-btn cel cel-2" to="/novaconsulta">Agendar Nova Consulta</router-link>
-      <router-link type="button" class="button medium-btn cel cel-3" to="/consultarstatus">Acompanhar Status do Exame</router-link>
-      <router-link type="button" class="button medium-btn cel cel-4" to="/clinicasprox">Consultar Clínicas Próximas</router-link>
-      <router-link type="button" class="button medium-btn cel cel-5" to="/atualizarcadastro">Atualização Cadastral</router-link>
-      <div class="cel cel-6">
-        <small-map id="mapsGoogle"/>
-      </div>
-    </div>
-    <footer class="footer-dashboard">
-      <p class="m-0">Sistema para Médicos &copy; 2022</p>
-    </footer>
-    <router-view></router-view>
-  </div>
+	<div id="containerAll">
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-fixed-top navbar-nav" style="background-color: #008894" id="navElement">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="#">
+						<router-link to="/">
+							<img src="@/assets/logo.png" alt="logo-sistema" width="24" height="24" class="d-inline-block align-text-top" />
+						</router-link>
+						MedCenter
+					</a>
+					<button
+						class="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse">
+						<div class="container-fluid" id="formContainer">
+							<form class="d-flex" role="search" @submit.prevent>
+								<input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search" />
+								<button class="btn btn-success">Buscar</button>
+							</form>
+						</div>
+						<div class="div-User d-flex align-items-center gap-2">
+							<p class="m-0">{{ user.user_metadata.nome ? user.user_metadata.nome : user.email }}</p>
+							<font-awesome-icon id="userIcon" icon="fa-solid fa-circle-user" />
+							<button class="btn btn-sm" id="signout-btn" @click="signOut">Sign-Out</button>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</header>
+		<div class="container mb-auto mt-auto" id="containerMainContent">
+			<router-link type="button" class="btn btn-primary btn-cel cel cel-1" to="/consultaspaciente">Minhas Consultas</router-link>
+			<router-link type="button" class="btn btn-primary btn-cel cel cel-2" to="/novaconsulta">Agendar Nova Consulta</router-link>
+			<router-link type="button" class="btn btn-primary btn-cel cel cel-3" to="/consultarstatus">Acompanhar Status do Exame</router-link>
+			<router-link type="button" class="btn btn-primary btn-cel cel cel-4" to="/clinicasprox">Consultar Clínicas Próximas</router-link>
+			<router-link type="button" class="btn btn-primary btn-cel cel cel-5" to="/atualizarcadastro">Atualização Cadastral</router-link>
+			<div class="cel cel-6">
+				<small-map id="mapsGoogle" />
+			</div>
+		</div>
+		<footer class="footer-dashboard">
+			<p class="m-0">Sistema para Médicos &copy; 2022</p>
+		</footer>
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
-  import SmallMap from '../../components/SmallMap.vue'
-  import useAuthUser from '@/useAuthUser';
+import SmallMap from "../../components/SmallMap.vue";
+import useAuthUser from "@/useAuthUser";
 
-  const { logout, user } = useAuthUser();
+const { logout, user } = useAuthUser();
 
-  export default {
-  name: 'App',
-  components: {
-    SmallMap
-  },
-  data() {
-    return {
-      user
-    };
-  },
-  methods: {
-    async signOut(){
-      try{
-        await logout();
-        this.$router.replace("/");
-      }
-      catch{
-        alert(error.error_description || error.message)
-        this.errorMessage = error.message;
-      }
-      finally{
-        alert("Logged Out")
-      }
-    },
-  }
-}
+export default {
+	name: "App",
+	components: {
+		SmallMap,
+	},
+	data() {
+		return {
+			user,
+		};
+	},
+	methods: {
+		async signOut() {
+			try {
+				await logout();
+				this.$router.replace("/");
+			} catch {
+				alert(error.error_description || error.message);
+				this.errorMessage = error.message;
+			} finally {
+				alert("Logged Out");
+			}
+		},
+	},
+};
 </script>
 
 <style scoped lang="scss">
+$success: #395b59;
+$primary: #008894;
 
-  $success: #395B59;
+@import "../../../node_modules/bootstrap/scss/bootstrap";
 
-  @import "../../../node_modules/bootstrap/scss/bootstrap";
+@import "@/css/DashBoardBaseStyle.scss";
 
-  header, div, footer {
-    font-family: 'Montserrat', sans-serif;
-  }
+@media (max-height: 900px) {
+	#containerAll {
+		#containerMainContent {
+			width: 70%;
 
-  #signout-btn {
-    color: rgb(236, 236, 236);
-    font-size: 0.7rem;
-    text-decoration: underline;
-  }
+			.cel-6 {
+				grid-row: 2/6;
+			}
 
-  #containerAll {
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    overflow: auto;
-    position: relative;
-    justify-content: center;
+			.btn-cel {
+				padding-top: 2rem;
+				padding-bottom: 2rem;
+			}
+		}
+	}
+}
 
-    &::before {
-      content: "";
-      position: fixed;
-      left: 0;
-      right: 0;
-      z-index: -1;
+@media (max-width: 1200px) {
+	#containerAll {
+		#containerMainContent {
+			.cel-6 {
+				grid-row: 2/6;
+			}
+		}
+	}
+}
 
-      display: block;
-      background-image: url(@/assets/backgroundDashboard.png);
-      background-size:cover;
-      width: 100%;
-      height: 100%;
+@media (max-width: 995px) {
+	#containerAll {
+		#containerMainContent {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			width: 100%;
+			padding-bottom: 3rem;
+			padding-top: 3rem;
 
-      -webkit-filter: blur(2px);
-      -moz-filter: blur(2px);
-      -o-filter: blur(2px);
-      -ms-filter: blur(2px);
-      filter: blur(2px);
-    }
+			.cel {
+				width: 90%;
+			}
 
-    #containerMainContent {
-      display: grid;
-      grid-template-columns: 1fr 1fr 1fr;
-      grid-template-rows: 1fr 1fr 1fr 1fr;
-      margin-left: auto;
-      margin-right: auto;
-      max-width: 50%;
-      align-items: center;
-      grid-gap: 1rem;
-      justify-content: center;
-      text-align: center;
+			.cel-6 {
+				height: 30rem;
+			}
+		}
 
-      .cel {
-        margin: 0;
-      }
-
-      .cel-5 {
-        grid-column: 1;
-      }
-
-      .cel-6 {
-        grid-column: 2/4;
-        grid-row: 2/5;
-        width: 100%;
-        height: 100%;
-        border: 3px solid #008894;
-      }
-    }
-  }
-
-  footer {
-    margin-top: 0;
-  }
-
-  .div-User {
-    #userIcon {
-      font-size: 2.4rem;
-      color: white;
-    }
-
-    p {
-      color: white;
-    }
-  }
-
-  #formContainer {
-    max-width: 50%;
-
-    form * {
-      border-radius: 0;
-    }
-  }
-
-  .minhas-consultas {
-    display: inline-block;
-    cursor: pointer;
-    width: 300px;
-    padding: 5px 10px;
-  }
-
-  .nova-consulta {
-    display: inline-block;
-    cursor: pointer;
-    width: 300px;
-    padding: 5px 10px;
-  }
-
-  .button {
-    display: inline-block;
-    background-color: #008894;
-    color: #FFFFFF;
-    cursor: pointer;
-    border-radius: 10px;
-    -moz-border-radius: 10px;
-    -webkit-border-radius: 10px;
-    }
-    
-    .small-btn {
-        width: 50px;
-        height: 25px;
-    }
-    
-    .medium-btn {
-        line-height: 100px;
-        text-align: center;
-        width: 300px;
-        height: 100px;
-    }
-    
-    .big-btn {
-        width: 620px;
-        height: 300px;
-    }
-
-  .footer-dashboard {
-    position: relative;
-    bottom: 0;
-    width: 100%;
-    height: 50px;
-    background-color: #008894;
-    color: #FFF;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .footer-dashboard p {
-    color: #EEEEEE;
-  }
+		&::before {
+			-webkit-filter: blur(10px);
+			-moz-filter: blur(10px);
+			-o-filter: blur(10px);
+			-ms-filter: blur(10px);
+			filter: blur(10px);
+		}
+	}
+}
 </style>
